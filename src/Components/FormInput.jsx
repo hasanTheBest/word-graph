b9d898cf29e183400197282b1d0b012b01620887
Mahmud <rainbowmahmud@gmail.com>
2023-08-23 21:15:39 +0600

    query parameters/filters /option to the word search form added

--- src/Components/FormInput.jsx
+++ src/Components/FormInput.jsx
@@ -9,15 +9,34 @@ const FormInput = () => {
             <option disabled selected>
               Pick Constraints
             </option>
-            <option>Means Like</option>
-            <option>Sounds Like</option>
-            <option>Spelled Like</option>
-            <option>Synonyms</option>
-            <option>Antonyms</option>
-            <option>Antonyms</option>
-            <option>Rhymes</option>
-            <option>Rhymes approx</option>
-            <option>Constant match</option>
+            <option value="ml">Means Like</option>
+            <option value="sl">Sounds Like</option>
+            <option value="sp">Spelled Like</option>
+            <option value="rel_syn">Synonyms</option>
+            <option value="rel_ant">Antonyms</option>
+            <option value="rel_hom">Homophones</option>
+            <option value="rel_cns">Consonant Match</option>
+            <option value="rel_rhy">Rhymes</option>
+            <option value="rel_nry">Rhymes Approx</option>
+            <option value="rel_jjb">Related Adjectives</option>
+            <option value="rel_jja">Related Nouns</option>
+            <optgroup>
+              <option value="rel_trg">
+                Statistically associated with (trigger)
+              </option>
+              <option value="rel_spc">Direct hypernyms (kind of)</option>
+              <option value="rel_gen">
+                Direct hyponyms (More general than)
+              </option>
+              <option value="rel_com">Direct holonyms (Comprises)</option>
+              <option value="rel_par">Direct meronyms (Part of)</option>
+              <option value="rel_bga">
+                Frequent follower (wreck-&gt;havoc)
+              </option>
+              <option value="rel_bgb">
+                Frequent predecessors (havoc-&gt;wreck)
+              </option>
+            </optgroup>
           </select>
           <div className="form-control">
             <div className="input-group">
