4fd5513262e2229f11590e9c68343bdd2e424794
Mahmud <rainbowmahmud@gmail.com>
2023-08-28 22:29:24 +0600

    whitespace to normal

--- src/Components/SearchResultItem.jsx
+++ src/Components/SearchResultItem.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 
 const SearchResultItem = ({ word: { word, defs, tags } }) => {
-  let meta = tags?.join(", ")
-  const definitions = defs?.map((def) => (
-    <li>{def}</li>
-  ))
+  let meta = tags?.join(", ");
+  const definitions = defs?.map((def) => <li>{def}</li>);
   return (
     <div className="stat place-items-center">
-      {meta && (<div className="stat-title">{meta} </div>)}
+      {meta && <div className="stat-title">{meta} </div>}
       <div className="stat-value">{word}</div>
-      {defs && (<div className="stat-desc">
-        <ol className="list-decimal">
-
-          {definitions}
-        </ol>
-      </div>)}
+      {defs && (
+        <div className="stat-desc whitespace-normal">
+          <ol className="list-decimal">{definitions}</ol>
+        </div>
+      )}
     </div>
   );
 };
